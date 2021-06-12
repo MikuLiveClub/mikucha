6ce638ab3a59f7e4db619041b20b4ea8b8cebb86
Colk-tech <colk.tech@gmail.com>
2021-06-13 00:54:11 +0900

    [add] Add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+DISCORD_TOKEN = environ["SELF_VCDIFF_TOKEN"]
+TARGET_CH_ID = environ["SELF_VCDIFF_TARGET_CH_ID"]
+ICON_URL = "https://cdn.discordapp.com/avatars/853288717215989780/1cf1b8159cbd89884d56532522b0fcf1.png"
+
+COOLDOWN_TIME = 15
